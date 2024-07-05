6f9a61bf11c1205061b9c79e936e37aaf63b0b5f
Tulece <antoine.porceddu@hotmail.ca>
2024-07-05 11:59:10 +0200

    start project

---
+++ includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anporced <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 16:57:44 by anporced          #+#    #+#             */
+/*   Updated: 2024/07/05 11:58:23 by anporced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
