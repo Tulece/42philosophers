9108053a92867dd539709b5d804f5df173514b1f
Tulece <antoine.porceddu@hotmail.ca>
2024-07-06 17:36:37 +0200

    time.c for ft_utsleep

---
+++ src/time.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anporced <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 17:09:59 by anporced          #+#    #+#             */
+/*   Updated: 2024/07/06 17:14:25 by anporced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+long	get_timestamp(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+void	custom_usleep(int milliseconds)
+{
+	long	start;
+
+	start = get_timestamp();
+	while ((get_timestamp() - start) < milliseconds)
+		usleep(10);
+}
